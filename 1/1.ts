8face6298d393ce9328ce098d44b857e56755fc0
Anders Heed <aheed@users.noreply.github.com>
2020-12-04 21:36:41 +0100

    add day1b

--- 1/1.ts
+++ 1/1.ts
@@ -25,15 +25,63 @@ export const day1 = () => {
     return "no solution found!";
 }
 
-/*export const day1alt = () => {
+const findSumElems = (sum: number, inp: number[]) : [number, number] => {
+
+    while (inp.length >= 2) {
+        const startNum =  inp.pop() ?? 0;
+
+        let secondTerm = inp.find(element => {
+            return (startNum + element == sum);
+        });
+
+        if (secondTerm) {
+            return [startNum, secondTerm];
+        } 
+    }
+
+    return [-1, -1];
+}
+
+export const day1alt = () => {
     const intext = readFileSync('./input/1.txt', 'utf-8');
 
     const rows = intext.split("\n");
     const in_numbers = rows.map(r => parseInt(r));
 
-    in_numbers.reduce(startNum => {
-        //Todo
-    })
+    const result = findSumElems(2020, in_numbers);
 
-    return "no solution found!";
-}*/
+    if (result[0] == -1) {
+        console.log("no solution found!");
+    }
+    else {
+        console.log(result);
+        console.log(result.reduce((prev, curr) => prev * curr));
+    }
+}
+
+export const day1b = () => {
+    const intext = readFileSync('./input/1.txt', 'utf-8');
+
+    const rows = intext.split("\n");
+    const in_numbers = rows.map(r => parseInt(r));
+
+    console.log("day1b");
+
+    while (in_numbers.length >= 3) {
+        const startNum = in_numbers.pop() ?? 0;
+        const numbers_tail = [...in_numbers];
+
+        let terms = findSumElems(2020 - startNum, numbers_tail);
+
+        if (terms[0] != -1) {
+            console.log("found a solution!");
+            terms.push(startNum);
+            console.log(terms);
+            console.log(terms.reduce((prev, curr) => prev * curr));
+            return;
+        }        
+    }
+ 
+    console.log("no solution found!");
+ 
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-import { day1 } from "./1/1";
+import { day1, day1alt, day1b } from "./1/1";
 
 day1();
+day1alt();
+day1b();
 
 
