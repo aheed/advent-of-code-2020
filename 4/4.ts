a1a91ca94e4475c1e59ba7768b26b59c2fe07634
aheed <anders.heed@combination.se>
2020-12-06 11:21:05 +0100

    day 4 completed

--- 4/4.ts
+++ 4/4.ts
@@ -36,8 +36,69 @@ const validIyr = (inp?: string) : boolean => validYear(2010, 2020, inp);
 const validEyr = (inp?: string) : boolean => validYear(2020, 2030, inp);
 
 const validHgt = (inp?: string) : boolean => {
-    return true;
+    if (!inp) {
+        return false;
+    }
+
+    let hgtMatch = inp.match(/(?<quantity>\d+)(?<unit>cm|in)/);
+    
+//    console.log(inp);
+//    console.log(hgtMatch);
+
+    let q = hgtMatch?.groups?.quantity ?? 0;
+    let u = hgtMatch?.groups?.unit ?? 'cm';
+//    console.log(q);
+//    console.log(u);
+
+    return (u == 'cm' && (q >= 150 && q <= 193)) || 
+           (u == 'in' && (q >= 59  && q <= 76));
 };
+
+const validHcl = (inp?: string) : boolean => {
+    if (!inp) {
+        return false;
+    }
+
+    let hclMatch = inp.match(/#(\d|[a-f]){6}/);
+    
+    // console.log("-");
+    // console.log(inp);
+    // console.log(hclMatch);
+
+    return !!hclMatch;
+
+};
+
+const validEcl = (inp?: string) : boolean => {
+    if (!inp) {
+        return false;
+    }
+
+    let eclMatch = inp.match(/^(amb|blu|brn|gry|grn|hzl|oth)$/);
+    
+    // console.log("-");
+    // console.log(inp);
+    // console.log(eclMatch);
+
+    return !!eclMatch;
+
+};
+
+const validPid  = (inp?: string) : boolean => {
+    if (!inp) {
+        return false;
+    }
+
+    let pidMatch = inp.match(/^\d{9}$/);
+    
+    // console.log("-");
+    // console.log(inp);
+    // console.log(pidMatch);
+
+    return !!pidMatch;
+
+};
+
 
 export const  day4b = () => {
     let input = getInputText('./input/4.txt');
@@ -49,13 +110,12 @@ export const  day4b = () => {
     .map(entry => new Map(entry))
     .filter(entry => 
         validByr(entry.get('byr')) &&
-        entry.has('iyr') &&
-        entry.has('eyr') &&
-        entry.has('hgt') &&
-        entry.has('hcl') &&
-        entry.has('ecl') &&
-        entry.has('pid'));
-    console.log(entries2[0]);
-    console.log(entries2.slice(0, 3));
+        validIyr(entry.get('iyr')) &&
+        validEyr(entry.get('eyr')) &&
+        validHgt(entry.get('hgt')) &&
+        validHcl(entry.get('hcl')) &&
+        validEcl(entry.get('ecl')) &&
+        validPid(entry.get('pid')));
+    //console.log(entries2.slice(0, 3));
     console.log(entries2.length);
 }
