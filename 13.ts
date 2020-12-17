e10330149044d633fa116aed52b35d01db2cad8d
aheed <anders.heed@combination.se>
2020-12-17 23:31:39 +0100

    day 13 completed

--- 13.ts
+++ 13.ts
@@ -1,3 +1,4 @@
+import { off } from "process";
 import { getInputRows } from "./utils/utils";
 
 export const day13 = () => {
@@ -7,7 +8,7 @@ export const day13 = () => {
     //console.log(rows);
 
     let currTime = parseInt(rows[0]);
-    console.log(currTime);
+    //console.log(currTime);
 
     let res = rows[1]
     .split(',')
@@ -15,6 +16,56 @@ export const day13 = () => {
     .map(m => parseInt(m))
     .map(m => [m, m - currTime  % m])
     .reduce((prev, curr) => curr[1] < prev[1] ? curr : prev);
-    console.log(res);
+    //console.log(res);
     console.log(res[0] * res[1]);
+}
+
+
+class Bus {
+    constructor(nr: number, offset:number) {
+        this.nr = nr;
+        this.offset = offset;
+    }
+    nr: number;
+    offset: number;
+}
+
+export const day13b = () => {
+
+    let rows = getInputRows('./input/13.txt');
+
+    let currTime = 0;
+
+    let buses = rows[1]
+    .split(',')
+    .map((bus: string, index:number): [string, number] => [bus, index])
+    .filter(bus => bus[0] != 'x')
+    .map(bus => new Bus(parseInt(bus[0]), bus[1]));
+
+    //console.log(buses);
+
+    let increment = 1;
+    let tstamp = buses[0].nr;
+    for (let busIndex = 0; busIndex < buses.length; ++busIndex)
+    {
+        let bus = buses[busIndex];
+        while ( (tstamp + bus.offset) % bus.nr != 0)
+        {
+            tstamp += increment;
+        }
+
+        let snapshot = tstamp;
+        tstamp += increment;
+        while ( (tstamp + bus.offset) % bus.nr != 0)
+        {
+            tstamp += increment;
+        }
+
+        increment = tstamp - snapshot;
+        tstamp = snapshot;
+
+        //console.log('a', busIndex, tstamp, increment);
+    }
+    console.log(tstamp);
+
 }
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { day1, day1alt, day1b } from "./1/1";
 import { day10 } from "./10/10";
 import { day11 } from "./11/11";
 import { day12 } from "./12";
-import { day13 } from "./13";
+import { day13, day13b } from "./13";
 import { day2, day2b } from "./2/2";
 import { day3, day3b } from "./3/3";
 import { day4, day4b } from "./4/4";
@@ -33,6 +33,7 @@ import { day9 } from "./9/9";
 //day11();
 //day12();
 day13();
+day13b();
 
 
 
