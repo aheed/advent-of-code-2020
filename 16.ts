fc9c2b369c8d3302f734c138815c88e58690f050
aheed <anders.heed@combination.se>
2020-12-19 23:23:14 +0100

    day 16 completed

--- 16.ts
+++ 16.ts
@@ -1,3 +1,4 @@
+import { constants } from "buffer";
 import { getInputRows } from "./utils/utils";
 
 class RuleRange {
@@ -23,6 +24,7 @@ class Rule {
 
     label: string;
     ranges: RuleRange[];
+    position?: number;
 }
 
 export const day16 = () => {
@@ -50,15 +52,15 @@ export const day16 = () => {
 
 
     /////////////
-    let v = 700;
-    let z = rules[0].isValid(v);
-    console.log(rules[0], v, z);
-    v = 45556667;
-    z = rules[0].isValid(v);
-    console.log(rules[0], v, z);
-    v = 901;
-    z = rules[5].isValid(v);
-    console.log(rules[5], v, z);
+    // let v = 700;
+    // let z = rules[0].isValid(v);
+    // console.log(rules[0], v, z);
+    // v = 45556667;
+    // z = rules[0].isValid(v);
+    // console.log(rules[0], v, z);
+    // v = 901;
+    // z = rules[5].isValid(v);
+    // console.log(rules[5], v, z);
 
     let errorRate = 0;
 
@@ -68,11 +70,70 @@ export const day16 = () => {
             let violatedRules = rules.filter(rule => !rule.isValid(ticketValue));
             if (violatedRules.length == rules.length) {
                 errorRate += ticketValue;
-                console.log(ticketValue, errorRate);
+                //console.log(ticketValue, errorRate);
             }
             //console.log(ticketValue);
         });
     });
 
     console.log(errorRate);
+
+
+    // Part 2
+    //////////////////////////
+
+    let validTickets = tickets.filter(ticket => {
+        let errorRate = 0;
+        ticket.forEach(ticketValue => {
+            let violatedRules = rules.filter(rule => !rule.isValid(ticketValue));
+            if (violatedRules.length == rules.length) {
+                errorRate += ticketValue;
+                //console.log(ticketValue, errorRate);
+            }
+            //console.log(ticketValue);
+        });
+        return errorRate == 0;
+    });
+    //console.log(validTickets);
+    //console.log(tickets.length, validTickets.length);
+
+
+    let knownPositions = new Map<number, Rule>();
+    let lastNofKnownPositions = -1;
+    //while (knownPositions.size < rules.length)
+    while (knownPositions.size != lastNofKnownPositions)
+    {
+        lastNofKnownPositions = knownPositions.size;
+        //console.log('   ', knownPositions.size);
+        for (let position=0; position<rules.length; ++position) {
+            let candidateRules = rules.filter(rule => {
+                let values = validTickets.map(ticket => ticket[position]);
+                let invalidValues = values.filter(val => !rule.isValid(val));
+                //console.log(rule);
+                //console.log(values);
+                //console.log(invalidValues);
+                return rule.position == undefined && invalidValues.length == 0;                
+            });
+            //console.log(position, 'candidates ', candidateRules.length);
+            if (candidateRules.length == 1) {
+                //console.log("got a match!");
+                //console.log(candidateRules[0].position, position);
+                candidateRules[0].position =  position;
+                knownPositions.set(position, candidateRules[0]);
+                //console.log(candidateRules[0].position, position);
+            }
+        }
+    }
+
+    //console.log(rules);
+
+    let departureRules = rules.filter(rule => rule.label.includes('departure'));
+    //console.log(departureRules);
+
+    let myTicket = myTicketRow.trim().split(',').map(s => parseInt(s));
+    let myTicketFields = departureRules.map(rule => myTicket[rule.position as number]);
+    //console.log(myTicketFields);
+
+    let res = myTicketFields.reduce((prev, curr) => prev * curr);
+    console.log(res);
 }
