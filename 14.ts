cebdd4f11cb4c805b436d397ce34ff328f511a79
aheed <anders.heed@combination.se>
2020-12-19 16:28:18 +0100

    day 14 part 1 completed

---
+++ 14.ts
@@ -0,0 +1,74 @@
+import { getInputRows } from "./utils/utils";
+
+type Op = 'mask' | 'mem';
+class Instruction {
+    constructor(operator: Op, addr:number, operand: string) {
+        this.operator = operator;
+        this.addr = addr;
+
+        if (operator === 'mask') {
+            let digits = operand.split('');
+            this.orMask = digits.map(d => d === '1' ? d : '0').map(d => parseInt(d));
+            this.andMask = digits.map(d => d === '0' ? d : '1').map(d => parseInt(d));
+        }
+        else {
+            this.memOperand = parseInt(operand);
+        }
+        this.operand = operand;
+    }
+
+    operator: Op;
+    addr: number;
+    memOperand?: number;
+    orMask?: number[];
+    andMask?: number[];
+    operand: string;
+}
+
+export const day14 = () => {
+
+    let rows = getInputRows('./input/14.txt');
+    
+    //console.log(rows[0]);
+
+    let instructions = rows.map(row => {
+        let [operation, operand] = row.split(' = ');
+
+        let match = operation.match(/(?<operation>mem|mask)(\[(?<addr>\d+)\])*/ );
+
+        return new Instruction(match?.groups?.operation as Op, parseInt(match?.groups?.addr ?? '0'), operand);
+
+    });
+
+    // console.log(instructions[0]);
+    // console.log(instructions[1]);
+
+    let memory = new Map<number, number>();
+    let orMask: number[] = [];
+    let andMask: number[] = [];
+
+    console.log('');
+    instructions.forEach(instruction => {
+        if (instruction.operator === 'mask') {
+            orMask = instruction.orMask as number[];
+            andMask = instruction.andMask as number[];
+        }
+        else {
+            let operandDigits =  (instruction?.memOperand as number).toString(2).split('').map(d => parseInt(d));
+            while (operandDigits.length < orMask.length) {
+                operandDigits = [0, ...operandDigits];
+            }
+            operandDigits = operandDigits.map( (d, index) => d & andMask[index]);
+            operandDigits = operandDigits.map( (d, index) => d | orMask[index]);
+            let val = parseInt(operandDigits.join(''), 2);
+            memory.set(instruction.addr, val);
+        }
+    });
+
+    // console.log('');
+    // console.log(memory);
+
+    let res = Array.from(memory.values()).reduce((prev, curr) => prev + curr);
+    console.log(res);
+}
+
--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { day10 } from "./10/10";
 import { day11 } from "./11/11";
 import { day12 } from "./12";
 import { day13, day13b } from "./13";
+import { day14 } from "./14";
 import { day2, day2b } from "./2/2";
 import { day3, day3b } from "./3/3";
 import { day4, day4b } from "./4/4";
@@ -32,8 +33,9 @@ import { day9 } from "./9/9";
 //day10();
 //day11();
 //day12();
-day13();
-day13b();
+// day13();
+// day13b();
+day14();
 
 
 
