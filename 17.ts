ae4592e0b166d8b8ed23b12dcba892370abe381c
aheed <anders.heed@combination.se>
2020-12-20 16:45:21 +0100

    day 17 completed

--- 17.ts
+++ 17.ts
@@ -8,7 +8,7 @@ export const day17 = () => {
     //console.log(rows);
 
     let space = [rows.map(row => row.trim().split('').map(c => c == '#'))];
-    console.log(space);
+    //console.log(space);
 
     const copySpace = (inSpace: boolean[][][]): boolean[][][] => inSpace.map(z => z.map(y => [...y]));
 
@@ -56,7 +56,7 @@ export const day17 = () => {
             });            
         });
 
-        console.log(xMaxGrowthNeeded, xMinGrowthNeeded, yMaxGrowthNeeded, yMinGrowthNeeded, zMaxGrowthNeeded, zMinGrowthNeeded);
+        //console.log(xMaxGrowthNeeded, xMinGrowthNeeded, yMaxGrowthNeeded, yMinGrowthNeeded, zMaxGrowthNeeded, zMinGrowthNeeded);
 
         if (xMaxGrowthNeeded) {
             space = space.map(z => z.map(y => [...y, false]));
@@ -130,13 +130,13 @@ export const day17 = () => {
             }
             else {
                 tmpSpace[z][y][x] = false;
-                console.log('deactivating', x, y, z);
+                //console.log('deactivating', x, y, z);
             }
         }
         else {
             if(activeNeighbors == 3) {
                 tmpSpace[z][y][x] = true;
-                console.log('activating', x, y, z);
+                //console.log('activating', x, y, z);
             }
             else {
                //stay inactive 
@@ -177,7 +177,159 @@ export const day17 = () => {
     }
 
 
-    console.log(space);
+    //console.log(space);
+    console.log(nofActive());
+
+}
+
+
+
+//////////////////////////////
+//////////////////////////////
+export const day17b = () => {
+
+    let rows = getInputRows('./input/17.txt');
+    //console.log(rows);
+
+    let space = [[rows.map(row => row.trim().split('').map(c => c == '#'))]];
+    //console.log(space);
+
+    const copySpace = (inSpace: boolean[][][][]): boolean[][][][] => inSpace.map(w => w.map(z => z.map(y => [...y])));
+
+    const growSpace = () => {
+
+        space = space.map(w => w.map(z => z.map(y => [...y, false])));
+        space = space.map(w => w.map(z => z.map(y => [false, ...y])));
+
+        space = space.map(w => w.map(z => {
+            let emptyrow = z[0].map(x => false);
+            return [...z, emptyrow];
+        }));
+
+        space = space.map(w => w.map(z => {
+            let emptyrow = z[0].map(x => false);
+            return [emptyrow, ...z];
+        }));
+
+        space = space.map(w => {
+            let emptySurface = w[0].map(y => y.map(x => false));
+            return [...w, emptySurface];
+        });
+
+        space = space.map(w => {
+            let emptySurface = w[0].map(y => y.map(x => false));
+            return [emptySurface, ...w];
+        });
+        
+
+        let emptyCube = space[0].map(z => z.map(y => y.map(x => false)));
+        space = [...space, emptyCube];
+
+        emptyCube = space[0].map(z => z.map(y => y.map(x => false)));
+        space = [emptyCube, ...space];
+    }
+
+    const inBounds = (x: number, y: number, z: number, w: number) : boolean =>
+        x >=0 && x < space[0][0][0].length &&
+        y >=0 && y < space[0][0].length &&
+        z >= 0 && z < space[0].length &&
+        w >= 0 && w < space.length;
+
+    let tmpSpace = copySpace(space);
+    const cycle = (x: number, y: number, z: number, w: number) => {
+        //console.log(x, y, z);
+        let active = space[w][z][y][x];
+        let activeNeighbors = 0;
+
+        for (let relx=-1; relx <= 1; ++relx) {
+            for (let rely=-1; rely <= 1; ++rely) {
+                for (let relz=-1; relz <= 1; ++relz) {
+                    for (let relw=-1; relw <= 1; ++relw) {
+                        if (relx == 0 && rely == 0 && relz == 0 && relw == 0) {
+                            continue;
+                        }
+
+                        let xs = x + relx;
+                        let ys = y + rely;
+                        let zs = z + relz;
+                        let ws = w + relw;
+
+                        if (!inBounds(xs, ys, zs, ws)) {
+                            continue;
+                        }
+
+                        // console.log(x, y, z, w);
+                        // console.log(xs, ys, zs, ws);
+                        if (space[ws][zs][ys][xs]) {
+                            ++activeNeighbors;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (active) {
+            if(activeNeighbors == 2 || activeNeighbors == 3) {
+                //stay active
+            }
+            else {
+                tmpSpace[w][z][y][x] = false;
+                //console.log('deactivating', x, y, z, w);
+            }
+        }
+        else {
+            if(activeNeighbors == 3) {
+                tmpSpace[w][z][y][x] = true;
+                //console.log('activating', x, y, z, w);
+            }
+            else {
+               //stay inactive 
+            }
+        }
+
+    }
+
+    const cycleSpace = () => {
+        for (let x=0; x < space[0][0][0].length; ++x) {
+            for (let y=0; y < space[0][0].length; ++y) {
+                for (let z=0; z < space[0].length; ++z) {
+                    for (let w=0; w < space.length; ++w) {
+                        cycle(x, y, z, w);
+                    }
+                }
+            }
+        }
+    }
+
+    const nofActive = () => {
+        let n = 0;
+        for (let x=0; x < space[0][0][0].length; ++x) {
+            for (let y=0; y < space[0][0].length; ++y) {
+                for (let z=0; z < space[0].length; ++z) {
+                    for (let w=0; w < space.length; ++w) {
+                        if (space[w][z][y][x]) {
+                            ++n;
+                        }
+                    }
+                }
+            }
+        }
+        return n;
+    }
+
+    for (let i = 0; i < 6; ++i) {
+        growSpace();    
+        tmpSpace = copySpace(space);
+        cycleSpace();
+        space = copySpace(tmpSpace);
+    }
+
+
+    // console.log(space);
+    // console.log(space[0]);
+    // growSpace();
+    // console.log(space);
+    // console.log(space[0]);
     console.log(nofActive());
 
 }
